ced0d5b96e960fc99c1b6daeeac16cbeeb98c804
SarpTekin <39226417+SarpTekin@users.noreply.github.com>
2025-11-12 10:18:52 +0100

    refactor: Update security configuration
    - Enable JwtProperties in UserServiceApplication
    - Add AuthenticationManager bean to SecurityConfig
    - Configure Spring Security for JWT authentication setup

--- user-service/src/main/java/com/microtodo/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/microtodo/user_service/UserServiceApplication.java
@@ -2,8 +2,12 @@ package com.microtodo.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.microtodo.user_service.config.JwtProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
+
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
